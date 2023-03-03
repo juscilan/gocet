0f2306d86d609f5a1643af220a3a492e9879dbab
Juscilan Moreto <juscilan.moreto@xogito.com>
2023-03-03 14:45:24 -0300

    first commit

--- cet_test.go
+++ cet_test.go
@@ -2,13 +2,15 @@ package gocet_test
 
 import (
 	"testing"
+
+	"github.com/juscilan/gocet"
 )
 
 func TestCalculateCET(t *testing.T) {
 
 	t.Run("It should not return an error", func(t *testing.T) {
 
-		cet := CalculateCET(400, 100, 200, 12)
+		cet := gocet.CalculateCET(400, 100, 200, 12)
 		expected := 948.98
 
 		if expected != cet {
